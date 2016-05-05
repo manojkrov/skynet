9561a643653fc4f429ae3f4db82845322c82feb7
srirammanoj <srirammanoj1@yahoo.co.in>
2016-05-05 19:08:04 +0530

    Improvements in the for loops as the code review comment here  http://codereview.stackexchange.com/questions/127494/thread-safe-bloom-filter-in-java 2.  adding int and long key support 3. Correcting a mistake in the BitSet initialization with incorrect size

--- bloomfilter/src/main/java/git/skynet/manoj/filter/BloomFilter.java
+++ bloomfilter/src/main/java/git/skynet/manoj/filter/BloomFilter.java
@@ -20,7 +20,7 @@ public class BloomFilter {
 	
 	public BloomFilter(int numberOfElemets, double falsePosProb) {
 		this.filterParams = BloomFilterCalculation.calculateOptimalParams(numberOfElemets, falsePosProb);
-		this.bitSet = new BitSet(filterParams.n * filterParams.m);
+		this.bitSet = new BitSet(filterParams.m);
 		lock = new ReentrantReadWriteLock();
 		readLock = lock.readLock();
 		writeLock = lock.writeLock();
@@ -75,15 +75,17 @@ public class BloomFilter {
 
 		writeLock.lock();
 		try {
+			int offset = 0;
+			long hash = hash1;
+			int index;
+			int mod;
+			
 			for (int i = 1; i <= filterParams.k; i++) {
-				long hash = hash1 + i * hash2;
-
-				int mod = ( int ) ( hash % filterParams.bucketLength );
-
-				int offset = (i - 1) * filterParams.bucketLength;
-
-				int index = offset + mod;
+				hash +=hash2;
+				mod = ( int ) ( hash % filterParams.bucketLength );
+				index = offset + mod;
 				bitSet.set(index);
+				offset += filterParams.bucketLength;
 			}
 			
 			elementsAdded++;
@@ -93,25 +95,53 @@ public class BloomFilter {
 		
 	}
 
+	public boolean containsElement(int element){
+		
+		return containsElement(new byte[] {
+            (byte)(element >>> 24),
+            (byte)(element >>> 16),
+            (byte)(element >>> 8),
+            (byte)(element) }
+				);
+	}
+
+	public boolean containsElement(long element){
+		return containsElement(new byte[] {
+				(byte)(element >>> 56),
+				(byte)(element >>> 48),
+				(byte)(element >>> 40),
+				(byte)(element >>> 32),
+	            (byte)(element >>> 24),
+	            (byte)(element >>> 16),
+	            (byte)(element >>> 8),
+	            (byte)(element) }
+					);
+	}
+
 	public boolean containsElement(String element) {
+		return containsElement(element.getBytes());
+	}
+	
+	public boolean containsElement(byte[] element) {
 		if (null == element) {
 			throw new NullPointerException();
 		}
 
-		long hash1 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element.getBytes(), 0, element.length(), filterParams.seed1));
-		long hash2 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element.getBytes(), 0, element.length(), filterParams.seed2));
+		long hash1 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed1));
+		long hash2 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed2));
 
 		readLock.lock();
 		try {
+			long hash = hash1;
+			int offset = 0;
+			int index;
+			int mod;
+
 			for (int i = 1; i <= filterParams.k; i++) {
-				long hash = hash1 + i * hash2;
-
-				int mod = ( int ) ( hash % filterParams.bucketLength );
-				int offset = (i - 1) * filterParams.bucketLength;
-				int index = offset + mod;
-				if (index < 0)
-					index = -index;
-
+				hash += hash2;
+				mod = ( int ) ( hash % filterParams.bucketLength );
+				index = offset + mod;
+				offset += filterParams.bucketLength;
 				if (!bitSet.get(index)) {
 					return false;
 				}
