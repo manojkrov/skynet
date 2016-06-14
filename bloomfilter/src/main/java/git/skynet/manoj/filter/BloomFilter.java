d9d8cbcb1c08f34a0af2be3161c800cda2c81c65
srirammanoj <srirammanoj1@yahoo.co.in>
2016-06-15 00:11:49 +0530

    Adding Parallel operations

--- bloomfilter/src/main/java/git/skynet/manoj/filter/BloomFilter.java
+++ bloomfilter/src/main/java/git/skynet/manoj/filter/BloomFilter.java
@@ -1,68 +1,44 @@
 package git.skynet.manoj.filter;
 
+import java.util.Arrays;
 import java.util.BitSet;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
-public class BloomFilter {
+public class BloomFilter<T> {
 
 	private BloomFilterCalculation.FilterParams filterParams;
 
 	private BitSet bitSet;
 	private int elementsAdded;
-	
+	private Function<T, byte[]> filterKey;
 	private final ReadWriteLock lock;
-	private final Lock readLock ;
+	private final Lock readLock;
 	private final Lock writeLock;
 
-	
-	
-	public BloomFilter(int numberOfElemets, double falsePosProb) {
+	public BloomFilter(Function<T, byte[]> key, int numberOfElemets, double falsePosProb) {
+
+		this.filterKey = key;
 		this.filterParams = BloomFilterCalculation.calculateOptimalParams(numberOfElemets, falsePosProb);
 		this.bitSet = new BitSet(filterParams.m);
 		lock = new ReentrantReadWriteLock();
 		readLock = lock.readLock();
 		writeLock = lock.writeLock();
 	}
-	
-	/** Converting an int into a byte array. This is the fastest way possible, instead of using Custom libraries or Byte buffers
-	 * 
-	 * @param element
-	 */
-	
-	public void addElement(int element){
-		
-		addElement(new byte[] {
-            (byte)(element >>> 24),
-            (byte)(element >>> 16),
-            (byte)(element >>> 8),
-            (byte)(element) }
-				);
-	}
 
-	
-	/** Converting a long into a byte array. This is the fastest way possible, instead of using Custom libraries or Byte buffers
+	/**
+	 * Converting into a byte array. This is the fastest way possible, instead
+	 * of using Custom libraries or Byte buffers
 	 * 
 	 * @param element
 	 */
 
+	public void addElement(T element) {
 
-	public void addElement(long element){
-		addElement(new byte[] {
-				(byte)(element >>> 56),
-				(byte)(element >>> 48),
-				(byte)(element >>> 40),
-				(byte)(element >>> 32),
-	            (byte)(element >>> 24),
-	            (byte)(element >>> 16),
-	            (byte)(element >>> 8),
-	            (byte)(element) }
-					);
-	}
-
-	public void addElement(String element){
-		addElement(element.getBytes());
+		addElement(filterKey.apply(element));
 	}
 
 	private void addElement(byte[] element) {
@@ -70,95 +46,86 @@ public class BloomFilter {
 			throw new NullPointerException();
 		}
 
-		long hash1 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed1));
-		long hash2 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed2));
+		long hash1 = Integer
+				.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed1));
+		long hash2 = Integer
+				.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed2));
+
+		final long hash = hash1;
+		int[] indexArray = new int[filterParams.k];
+
+		IntStream.range(1, filterParams.k + 1)
+					.parallel().forEach(
+					i -> indexArray[i - 1] = i * filterParams.bucketLength + (int) ((hash + i * hash2) % filterParams.bucketLength)
+				);
 
 		writeLock.lock();
 		try {
-			int offset = 0;
-			long hash = hash1;
-			int index;
-			int mod;
-			
-			for (int i = 1; i <= filterParams.k; i++) {
-				hash +=hash2;
-				mod = ( int ) ( hash % filterParams.bucketLength );
-				index = offset + mod;
-				bitSet.set(index);
-				offset += filterParams.bucketLength;
-			}
-			
+			Arrays.stream(indexArray).parallel().forEach(bitSet::set);
 			elementsAdded++;
 		} finally {
 			writeLock.unlock();
 		}
-		
 	}
 
-	public boolean containsElement(int element){
-		
-		return containsElement(new byte[] {
-            (byte)(element >>> 24),
-            (byte)(element >>> 16),
-            (byte)(element >>> 8),
-            (byte)(element) }
-				);
+	public boolean containsElement(T element) {
+
+		return containsElement(filterKey.apply(element));
 	}
 
-	public boolean containsElement(long element){
-		return containsElement(new byte[] {
-				(byte)(element >>> 56),
-				(byte)(element >>> 48),
-				(byte)(element >>> 40),
-				(byte)(element >>> 32),
-	            (byte)(element >>> 24),
-	            (byte)(element >>> 16),
-	            (byte)(element >>> 8),
-	            (byte)(element) }
-					);
-	}
-
-	public boolean containsElement(String element) {
-		return containsElement(element.getBytes());
-	}
-	
 	public boolean containsElement(byte[] element) {
 		if (null == element) {
 			throw new NullPointerException();
 		}
 
-		long hash1 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed1));
-		long hash2 = Integer.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed2));
+		long hash1 = Integer
+				.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed1));
+		long hash2 = Integer
+				.toUnsignedLong(Murmur3Hash.murmurhash3_x86_32(element, 0, element.length, filterParams.seed2));
+
+		final long hash = hash1;
+		boolean result;
+		int[] indexArray = new int[filterParams.k];
+
+		IntStream.range(1, filterParams.k + 1)
+		.parallel().forEach(
+		i -> indexArray[i - 1] = i * filterParams.bucketLength + (int) ((hash + i * hash2) % filterParams.bucketLength)
+	);
 
 		readLock.lock();
 		try {
-			long hash = hash1;
-			int offset = 0;
-			int index;
-			int mod;
+			result = Arrays.stream(indexArray).parallel().allMatch(bitSet::get);
 
-			for (int i = 1; i <= filterParams.k; i++) {
-				hash += hash2;
-				mod = ( int ) ( hash % filterParams.bucketLength );
-				index = offset + mod;
-				offset += filterParams.bucketLength;
-				if (!bitSet.get(index)) {
-					return false;
-				}
-			}
 		} finally {
 			readLock.unlock();
 		}
 
-		return true;
+		return result;
 	}
 
 	public int getElementsAdded() {
 		return elementsAdded;
 	}
-	
-	public void clearFilter(){
-		bitSet.clear();
+
+	public int getCardinality() {
+		int cardinality = -1;
+		readLock.lock();
+		try {
+			cardinality = bitSet.cardinality();
+		} finally {
+			readLock.unlock();
+		}
+		return cardinality;
+	}
+
+	public void clearFilter() {
+		writeLock.lock();
+		try {
+			bitSet.clear();
+		} finally {
+			writeLock.unlock();
+		}
+
 	}
 
 	@Override
--- bloomfilter/src/test/java/git/skynet/manoj/filter/test/BloomFilterTest.java
+++ bloomfilter/src/test/java/git/skynet/manoj/filter/test/BloomFilterTest.java
@@ -1,15 +1,17 @@
 package git.skynet.manoj.filter.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import git.skynet.manoj.filter.BloomFilter;
+import git.skynet.manoj.filter.FilterKeys;
 
 /**
  * @author manoj
@@ -17,15 +19,16 @@ import git.skynet.manoj.filter.BloomFilter;
  */
 public class BloomFilterTest {
 
-	private static BloomFilter bloomFilter;
+	private static BloomFilter<Integer> bloomFilter;
 	private static Random random = new Random();
 	private static int TOTAL_NUMBER_OF_ELEMENTS = 10000;
 	private static double FALSE_POSITIVE_PROBABILITY = 0.01;
 	private static int TEST_INPUT_SIZE = 1000;
+	private static int[] input = new int[TEST_INPUT_SIZE]; 
 	
 	@BeforeClass
 	public static void setUpBeforeClass() {
-		bloomFilter = new BloomFilter(TOTAL_NUMBER_OF_ELEMENTS, FALSE_POSITIVE_PROBABILITY);
+		bloomFilter = new BloomFilter<Integer>(FilterKeys.INTEGER_KEY_TYPE,TOTAL_NUMBER_OF_ELEMENTS, FALSE_POSITIVE_PROBABILITY);
 	}
 
 	@Before
@@ -35,17 +38,14 @@ public class BloomFilterTest {
 	
 	@Test
 	public void testInsertedElements() {
-		int[] input = new int[TEST_INPUT_SIZE];
 		IntStream.rangeClosed(0, TEST_INPUT_SIZE-1).forEach(i -> { input[i]= random.nextInt(1000000);bloomFilter.addElement(input[i]);});
-		
 		Arrays.stream(input).forEach(i -> assertTrue(bloomFilter.containsElement(i)));
 	}
 	
 	@Test
 	public void testFalsePositiveRate() {
-		int[] input = new int[TEST_INPUT_SIZE];
-		IntStream.rangeClosed(0, TEST_INPUT_SIZE-1).forEach(i -> { input[i]= random.nextInt(1000000);bloomFilter.addElement(input[i]);});
 		
+		IntStream.rangeClosed(0, TEST_INPUT_SIZE-1).forEach(i -> { input[i]= random.nextInt(1000000);bloomFilter.addElement(input[i]);});
 		boolean[] results = new boolean[1000];
 		IntStream.rangeClosed(0, TEST_INPUT_SIZE-1).forEach(i -> { results[i] = bloomFilter.containsElement(-input[i]); });
 		
@@ -56,5 +56,27 @@ public class BloomFilterTest {
 		
 		assertTrue(" False positive ratio should be less than  "+ FALSE_POSITIVE_PROBABILITY , positives/TEST_INPUT_SIZE < FALSE_POSITIVE_PROBABILITY);
 	}
+	
+	@Test
+	public void testMultithreadedAccess(){
+		IntStream.rangeClosed(1, 100).forEach( j -> 
+												{	new Thread( () -> 
+																		IntStream.rangeClosed(0, TEST_INPUT_SIZE-1).forEach(
+																				i -> 
+																					{	
+																						input[i]= random.nextInt(1000000);
+																						bloomFilter.addElement(input[i]);
+																						bloomFilter.containsElement(input[i]);
+																					} 
+																				)
+															  ).start(); 
+												}
+											 );
+		
+	
+	
+	}
+	
+	
 	
 }
